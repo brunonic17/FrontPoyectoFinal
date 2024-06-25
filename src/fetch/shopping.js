828431386e2cd275f925b106077fec51026dcbe9
delprafede <delpradofederico0@gmail.com>
2024-06-25 00:33:26 -0300

    errores en carrito

--- src/fetch/shopping.js
+++ src/fetch/shopping.js
@@ -18,7 +18,7 @@ const GetShoppings = async ()=> {
 }
 
 
-export const PostShoppings = async (Cart)=> {
+ const PostShoppings = async (Cart)=> {
         let CartJson=JSON.stringify(Cart)
 
     const response=await fetch(`http://localhost:5050/api/carrito`,{
@@ -38,61 +38,61 @@ export const PostShoppings = async (Cart)=> {
         return data
 }
 
-// const GetIdUsu = async (Cart)=> {
-//     let CartJson=JSON.stringify(Cart)
+const GetIdUsu = async (Cart)=> {
+    let CartJson=JSON.stringify(Cart)
 
-// const response=await fetch(`http://localhost:5000/api/carrito/IdUsu`,{
-//     method:"POST",
-//     body:CartJson ,
-//     headers:{
-//         "content-type":"application/json"
+const response=await fetch(`http://localhost:5000/api/carrito/IdUsu`,{
+    method:"POST",
+    body:CartJson ,
+    headers:{
+        "content-type":"application/json"
        
-//     },
+    },
 
   
     
-// });
+});
 
-//     const data=response.json();
+    const data=response.json();
 
-//     return data
-// }
+    return data
+}
 
-// const DeleteProduct = async (Product)=> {
-//     let ProductJson=JSON.stringify(Product)
+const DeleteProduct = async (Product)=> {
+    let ProductJson=JSON.stringify(Product)
 
-//     const response=await fetch(`http://localhost:5000/api/carrito`,{
-//     method:"DELETE",
-//     body:ProductJson ,
-//     headers:{
-//         "content-type":"application/json"
+    const response=await fetch(`http://localhost:5000/api/carrito`,{
+    method:"DELETE",
+    body:ProductJson ,
+    headers:{
+        "content-type":"application/json"
        
-//     },
-//     });
+    },
+    });
 
-//     const data=response.json();
+    const data=response.json();
 
-//     return data
-// }
+    return data
+}
 
-// const PagoPay = async (Carrito)=> {
-//     let CarritoJson=JSON.stringify(Carrito)
+const PagoPay = async (Carrito)=> {
+    let CarritoJson=JSON.stringify(Carrito)
 
-//     const response=await fetch(`http://localhost:5000/api/carrito/confirma`,{
-//     method:"POST",
-//     body:CarritoJson ,
-//     headers:{
-//         "content-type":"application/json"
+    const response=await fetch(`http://localhost:5000/api/carrito/confirma`,{
+    method:"POST",
+    body:CarritoJson ,
+    headers:{
+        "content-type":"application/json"
        
-//     },
+    },
 
   
     
-// });
+});
 
-//     const data=response.json();
+    const data=response.json();
 
-//     return data
-// }
+    return data
+}
 
-// export { GetShoppings,PostShoppings,DeleteProduct,PagoPay,GetIdUsu}
+export { GetShoppings,PostShoppings,DeleteProduct,PagoPay,GetIdUsu}
